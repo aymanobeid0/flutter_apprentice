63d1cfc118c3948119c5f47018a8d770fe71bdc1
aymanobeid0 <aymanobeid0@gmail.com>
2022-05-17 18:39:01 +0000

    navigation

---
+++ routes_and_navigation/lib/navigation/app_router.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+  final GroceryManager groceryManager;
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+// TODO: Add Listeners
+  }
+// TODO: Dispose listeners
+
+// TODO: Dispose listeners
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      // TODO: Add onPopPage
+      pages: [
+        // TODO: Add SplashScreen
+        // TODO: Add LoginScreen
+        // TODO: Add OnboardingScreen
+        // TODO: Add Home
+        // TODO: Create new item
+        // TODO: Select GroceryItemScreen
+        // TODO: Add Profile Screen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+  // TODO: Add _handlePopPage
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+    // TODO: Handle Onboarding and splash
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
